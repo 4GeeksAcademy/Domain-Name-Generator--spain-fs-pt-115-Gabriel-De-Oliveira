07879609895a0ffcfc7aa1b8e4705945f3f1eac5
gabrieeel2109 <gabrielof219@icloud.com>
2025-06-28 10:22:43 +0000

    faltan cambios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("");
 };
+
+const pronoun = ['the', 'our',"back"];
+const adj = ['great', 'big',"bitcoin"];
+const noun = ['jogger', 'racoon', "USDT"];
+const extensions = [`.com`, `.net`, `.us`, `.io`, ".es"]
+const grupo = {};
+
+extensions.forEach(ext => grupo[ext] = [])
+
+pronoun.forEach((pron) => {
+  adj.forEach((ad) => {
+    noun.forEach((nou) => {
+      extensions.forEach((ext) => {
+        const element = `${pron}${ad}${nou}${ext}`;
+        grupo[ext].push(element);
+        
+      });
+    });
+  });
+});
+
+for (let ext in grupo) {
+  for (let i = 0; i < grupo[ext].length; i++) {
+   console.log(grupo);
+   
+  };
+};
+
+
+
+
+
+
+
